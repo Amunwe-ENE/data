9fd4b8e70711a5f3719b0e13504eb6dcb2cb9ea8
amunwe-ene <eneamunwe@gmail.com>
2020-08-15 03:34:53 +0200

    added autoreconnect to options

--- schema.js
+++ schema.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
-//const option = { socketTimeoutMS: 30000, keepAlive: true, reconnectTries: 30000 };
-mongoose.connect(process.env.MONGODB_URL).then(
+const option = { socketTimeoutMS: 30000,
+   keepAlive: true, 
+   reconnectTries: 30000 ,
+  autoReconnect: true,
+   poolSize: 10, // Maintain up to 10 socket connections
+   //serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+   socketTimeoutMS: 405000, // Close sockets after 45 seconds of inactivity
+   family: 4 // Use IPv4, skip trying IPv6
+  };
+mongoose.connect(process.env.MONGODB_URL, option).then(
     () => {
       console.log('Ready to Use...Connected successfully');
     },
