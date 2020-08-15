c4a8f68a2c69206804d48dbd8eb9414fcdfa6afe
amunwe-ene <eneamunwe@gmail.com>
2020-08-15 03:34:30 +0200

    addel counter

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const apiURL = 'https://api.covid19api.com/all';
 const tURL = 'https://api.covid19api.com/total/country/'
 let counter = 0;
  const countList  = [
- //{  country : "Saint Kitts and Nevis",   code : "KN" }, 
- {  country : "Saint Lucia",   code : "LC" }, {  country : "Saint Vincent and Grenadines",   code : "VC" }, {  country : "San Marino",   code : "SM" }, {  country : "Sao Tome and Principe",   code : "ST" }, {  country : "Saudi Arabia",   code : "SA" }, {  country : "Senegal",   code : "SN" }, {  country : "Serbia",   code : "RS" }, {  country : "Seychelles",   code : "SC" }, {  country : "Sierra Leone",   code : "SL" }, {  country : "Singapore",   code : "SG" }, {  country : "Slovakia",   code : "SK" }, {  country : "Slovenia",   code : "SI" }, {  country : "Somalia",   code : "SO" }, {  country : "South Africa",   code : "ZA" }, {  country : "South Sudan",   code : "SS" }, {  country : "Spain",   code : "ES" }, {  country : "Sri Lanka",   code : "LK" }, {  country : "Sudan",   code : "SD" }, {  country : "Suriname",   code : "SR" }, {  country : "Swaziland",   code : "SZ" }, {  country : "Sweden",   code : "SE" }, {  country : "Switzerland",   code : "CH" }, {  country : "Syrian Arab Republic (Syria)",   code : "SY" }, {  country : "Taiwan, Republic of China",   code : "TW" }, {  country : "Tajikistan",   code : "TJ" }, {  country : "Tanzania, United Republic of",   code : "TZ" }, {  country : "Thailand",   code : "TH" }, {  country : "Timor-Leste",   code : "TL" }, {  country : "Togo",   code : "TG" }, {  country : "Trinidad and Tobago",   code : "TT" }, {  country : "Tunisia",   code : "TN" }, {  country : "Turkey",   code : "TR" }, {  country : "Uganda",   code : "UG" }, {  country : "Ukraine",   code : "UA" }, {  country : "United Arab Emirates",   code : "AE" }, {  country : "United Kingdom",   code : "GB" }, {  country : "United States of America",   code : "US" }, {  country : "Uruguay",   code : "UY" }, {  country : "Uzbekistan",   code : "UZ" }, {  country : "Venezuela (Bolivarian Republic)",   code : "VE" }, {  country : "Viet Nam",   code : "VN" }, {  country : "Western Sahara",   code : "EH" }, {  country : "Yemen",   code : "YE" }, {  country : "Zambia",   code : "ZM" }, {  country : "Zimbabwe",   code : "ZW" },
+ {  country : "Saint Kitts and Nevis",   code : "KN" }, 
+ //{  country : "Saint Lucia",   code : "LC" }, {  country : "Saint Vincent and Grenadines",   code : "VC" }, {  country : "San Marino",   code : "SM" }, {  country : "Sao Tome and Principe",   code : "ST" }, {  country : "Saudi Arabia",   code : "SA" }, {  country : "Senegal",   code : "SN" }, {  country : "Serbia",   code : "RS" }, {  country : "Seychelles",   code : "SC" }, {  country : "Sierra Leone",   code : "SL" }, {  country : "Singapore",   code : "SG" }, {  country : "Slovakia",   code : "SK" }, {  country : "Slovenia",   code : "SI" }, {  country : "Somalia",   code : "SO" }, {  country : "South Africa",   code : "ZA" }, {  country : "South Sudan",   code : "SS" }, {  country : "Spain",   code : "ES" }, {  country : "Sri Lanka",   code : "LK" }, {  country : "Sudan",   code : "SD" }, {  country : "Suriname",   code : "SR" }, {  country : "Swaziland",   code : "SZ" }, {  country : "Sweden",   code : "SE" }, {  country : "Switzerland",   code : "CH" }, {  country : "Syrian Arab Republic (Syria)",   code : "SY" }, {  country : "Taiwan, Republic of China",   code : "TW" }, {  country : "Tajikistan",   code : "TJ" }, {  country : "Tanzania, United Republic of",   code : "TZ" }, {  country : "Thailand",   code : "TH" }, {  country : "Timor-Leste",   code : "TL" }, {  country : "Togo",   code : "TG" }, {  country : "Trinidad and Tobago",   code : "TT" }, {  country : "Tunisia",   code : "TN" }, {  country : "Turkey",   code : "TR" }, {  country : "Uganda",   code : "UG" }, {  country : "Ukraine",   code : "UA" }, {  country : "United Arab Emirates",   code : "AE" }, {  country : "United Kingdom",   code : "GB" }, {  country : "United States of America",   code : "US" }, {  country : "Uruguay",   code : "UY" }, {  country : "Uzbekistan",   code : "UZ" }, {  country : "Venezuela (Bolivarian Republic)",   code : "VE" }, {  country : "Viet Nam",   code : "VN" }, {  country : "Western Sahara",   code : "EH" }, {  country : "Yemen",   code : "YE" }, {  country : "Zambia",   code : "ZM" }, {  country : "Zimbabwe",   code : "ZW" },
 ] 
 
 const getAndSaveAll = () => {
@@ -17,6 +17,8 @@ const getAndSaveAll = () => {
         console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
       //  console.log('body:', body); // Print the HTML for the Google homepage.
         let json = JSON.parse(body)
+        let count = 0
+        console.log('Total: '+json.length)
         json.map(data => {
         new All({
           Country: data.Country,
@@ -31,7 +33,8 @@ const getAndSaveAll = () => {
         LocationID: data.LocationID
         }).save((err, results)=> {
           if(err) console.error(err)
-          console.log(`Successfully save ${results.Country}`)
+          count +=1
+          console.log(`k${count}: Successfully save ${results.Country}`)
         })
       })
       });
@@ -50,6 +53,8 @@ const getAndSaveDaily = (code) => {
       console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
     //  console.log('body:', body); // Print the HTML for the Google homepage.
       let json = JSON.parse(body)
+      let count = 0
+        console.log('Total: '+json.length)
       json.map(data => {
       new Daily({
         Country: data.Country,
@@ -67,7 +72,7 @@ const getAndSaveDaily = (code) => {
 
       }).save((err, results)=> {
         if(err) console.error(err)
-        console.log(`Successfully save ${results.Country}`)
+        console.log(`${count}: Successfully save ${results.Country}`)
       })
     })
     });
